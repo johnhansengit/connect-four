9bd09b5e1607973b30c1139b689e7dba12c009f7
John Hansen <jhb.rgn@gmail.com>
2023-11-17 12:48:44 -0500

    Correct HTML/CSS and create functions/caches

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 /*----- constants -----*/
 
+const COLORS = {
+    '0': 'white',
+    '1': 'purple',
+    '-1': 'orange'
+};
 
 /*----- state variables -----*/
 
@@ -10,6 +15,9 @@ let winner; // null = no winner; 1 or -1 = winner; 'T' = tie game
 
 /*----- cached elements  -----*/
 
+const messageEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
+const markerEls = document.querySelectorAll('#markers > div');
 
 /*----- event listeners -----*/
 
@@ -34,10 +42,48 @@ function init() {
     render();
 }
 
+// Visualize all state in the DOM
 function render() {
+    renderBoard();
+    renderMessage();
+    // Hide/show UI elements (controls)
+    renderControls();
+}
 
+function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+        // Iterate over the cells in the current column (colArr)
+        colArr.forEach(function(cellVal, rowIdx) {
+            const cellId = `c${colIdx}r${rowIdx}`;
+            const cellEl = document.getElementById(cellId);
+            cellEl.style.backgroundColor = COLORS[cellVal];
+        });
+    });
+}
+
+function renderMessage() {
+    if (winner === 'T') {
+        messageEl.innerText = 'Tie game!';
+    } else if (winner) {
+        messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[winner].toUpperCase()}</span> Wins!`;
+    } else {
+        // Game is in play
+        messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[turn].toUpperCase()}</span>'s Turn`;
+    }
+}
+
+function renderControls() {
+    // Ternary expression is the go-to when you want one of two values returned
+    // conditional-expression ? truthy-expression : falsy-expression
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+    // Iterate over marker elements to hide or show according to column being full (no 0s) or not
+    markerEls.forEach(function(markerEl, colIdx) {
+        const hideMarker = !board[colIdx].includes(0) || winner;
+        markerEl.style.visibility = hideMarker ? 'hidden' : 'visible';
+    });
 }
 
 /* TO DO
     - Randomize which player gets to go first
+    - Try to 'animate' (blink through) disks falling from markers?
 */
