55df129ccfc46c99c461c069e28d680c0ebd4794
John Hansen <jhb.rgn@gmail.com>
2023-11-17 11:32:07 -0500

    JS state variables and initial functions

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 
 /*----- state variables -----*/
 
+let board; // array of 7 column arrays
+let turn; // 1 or -1
+let winner; // null = no winner; 1 or -1 = winner; 'T' = tie game
+
 
 /*----- cached elements  -----*/
 
@@ -11,3 +15,29 @@
 
 
 /*----- functions -----*/
+init();
+
+// Intialize all state, then call render()
+function init() {
+    // To visualize the board's mapping to the DOM, rotate the board array 90 degrees counter-clockwise
+    board = [
+        [0, 0, 0, 0, 0, 0], // col 0
+        [0, 0, 0, 0, 0, 0], // col 1
+        [0, 0, 0, 0, 0, 0], // col 2
+        [0, 0, 0, 0, 0, 0], // col 3
+        [0, 0, 0, 0, 0, 0], // col 4
+        [0, 0, 0, 0, 0, 0], // col 5
+        [0, 0, 0, 0, 0, 0], // col 6
+    ]
+    turn = 1;
+    winner = null;
+    render();
+}
+
+function render() {
+
+}
+
+/* TO DO
+    - Randomize which player gets to go first
+*/
